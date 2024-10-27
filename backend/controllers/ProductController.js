0d96d676539b32ace74e72fbfc1ac4ce07f0de05
syahrulramadhon <syahrulramadhon439@gmail.com>
2024-10-27 18:52:24 +0700

    update & delete image

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,5 +1,6 @@
 import Product from "../models/ProductModel.js";
 import path from "path";
+import fs from "fs";
 
 export const getProducts = async (req, res) => {
     try {
@@ -52,10 +53,73 @@ export const saveProduct = (req, res) => {
     })
 }
 
-export const updateProduct = (req, res) => {
+export const updateProduct = async (req, res) => {
+    const product = await Product.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
 
+    if (!product) return res.status(404).json({ msg: 'No Data Found' });
+
+    let fileName = "";
+    if (req.files === null) {
+        fileName = product.image;
+    } else {
+        const file = req.files.file;
+        const fileSize = file.data.length;
+        const ext = path.extname(file.name);
+        fileName = file.md5 + ext;
+        const allowedType = ['.png', '.jpg', '.jpeg'];
+
+        if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ msg: 'Invalid Image Extension' });
+        if (fileSize > 5000000) return res.status(422).json({ msg: "Image must be less than 5 MB" });
+        
+        const filepath = `./public/images/${product.image}`;
+        fs.unlinkSync(filepath);
+
+        file.mv(`./public/images/${fileName}`, (err) => {
+            if (err) return res.status(500).json({ msg: err.message });
+        });
+        
+    }
+
+    const name = req.body.title;
+    const url = `${req.protocol}://${req.get('host')}/images/${fileName}`;
+
+    try {
+        await Product.update({ name: name, image: fileName, url: url }, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ msg: 'Product Updated' });
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-export const deleteProduct = (req, res) => {
+export const deleteProduct = async (req, res) => {
+    const product = await Product.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    if (!product) return res.status(404).json({ msg: 'No Data Found' });
+
+    try {
+        const filepath = `./public/images/${product.image}`;
+        fs.unlinkSync(filepath);
+        await Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ msg: 'Product Deleted' });
+    } catch (error) {
+        console.log(error.message);
+    }
+        
 
 }
--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(express.static("public"));
 app.use(ProductRoute);
 
 app.listen(5000, () => { console.log("Server started on port 5000") });
